48ecf53a8446a5b3705a5512350e566a0ed14469
Mol0k <cristiansmurfcsgo@gmail.com>
2022-05-23 18:01:50 +0200

    Commit Add watchlist
    Añadir la funcionalidad para que los usuarios puedan añadir las películas que ya han visto.

---
+++ www/add_to_watchlist.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 'On');
+require __DIR__ . '/../php_util/db_connection.php';
+
+session_start();
+$mysqli = get_db_connection_or_die();
+$user_id = $_SESSION['user_id'];
+if(!isset($_SESSION['user_id']) & empty($_SESSION['user_id'])){
+		header('location: login.php');
+	}
+if(isset($_GET['id']) & !empty($_GET['id'])){
+	$movie_id = $_GET['id'];
+	echo $sql = "INSERT INTO twatchlist (movie_id, usuario_id) VALUES ($movie_id, $user_id)";
+	$res = mysqli_query($mysqli, $sql);
+	if($res){
+		header('location: watchlist.php');
+		//echo "redirect to wish list page";
+	}
+}else{
+	header('location: watchlist.php');
+}
+
+?>
---
+++ www/watchlist.php
@@ -0,0 +1,32 @@
+<?php
+ini_set('display_errors', 'On');
+require __DIR__ . '/../php_util/db_connection.php';
+
+session_start();
+$mysqli = get_db_connection_or_die();
+$user_id = $_SESSION['user_id'];
+
+?>
+
+
+<div>
+
+
+<?php
+        
+		$query3 = 'SELECT * FROM tmovie INNER JOIN twatchlist ON tmovie.id = twatchlist.movie_id INNER JOIN tuser ON twatchlist.usuario_id = tuser.id; ';
+       
+		$res = mysqli_query($mysqli, $query3);
+		while($fila = mysqli_fetch_assoc($res)){
+	?>
+					<tr>
+						<td>
+							
+						</td>
+                        <td><?php echo $fila['title']; ?></td>
+						
+					</tr>
+				<?php } ?>
+
+
+</div>
