7546bf99b888fe139f7549784fbb4da44eaca495
Mol0k <cristiansmurfcsgo@gmail.com>
2022-05-23 18:00:24 +0200

    Commit comentarios
    Añadir la funcionalidad para que los usuarios puedan comentar en una película y dar su opinión.

--- www/comment.php
+++ www/comment.php
@@ -4,31 +4,29 @@ require __DIR__ . '/../php_util/db_connection.php';
 
 // session_start();
 $mysqli = get_db_connection_or_die();
-?>
-<html>
-    <body>
-      <?php
-        session_start();
-        $user_id_a_insertar = 'NULL';
-        if (!empty($_SESSION['user_id'])) {
-        $user_id_a_insertar = $_SESSION['user_id'];
-        }
-        $movie_id = $_POST['movie_id'];
-        $comentario = $_POST['new_comment'];
-        $date = date('Y-m-d');
 
-        $query = "INSERT INTO tComentarios(comentario, movie_id, usuario_id, fecha_comentario)
-        VALUES ('".$comentario."',".$movie_id.",".$user_id_a_insertar.",'".$date."')";
-        mysqli_query($mysqli, $query) or die('Error');
-        echo "<p>Nuevo comentario ";
-        echo mysqli_insert_id($mysqli);
-        echo " añadido</p>";
-        echo "<p>Nueva fecha";
-        echo " añadida</p>";
-        echo "<p>Id usuario";
-        echo " añadido</p>";
-        echo "<a href='/movies.php?id=".$movie_id."'>Volver</a>";
-        mysqli_close($mysqli);
-      ?>
-    </body>
-</html>
+  session_start();
+  $user_id_a_insertar = 'NULL';
+  if (!empty($_SESSION['user_id'])) {
+  $user_id_a_insertar = $_SESSION['user_id'];
+  }
+  $movie_id = $_POST['movie_id'];
+  $comentario = $_POST['new_comment'];
+  $date = date('Y-m-d h:i:sa');
+  if(!empty($_POST["new_comment"])){
+  
+  $query = "INSERT INTO tComentarios(comentario, movie_id, usuario_id, fecha_comentario)
+  VALUES ('".$comentario."',".$movie_id.",".$user_id_a_insertar.",'".$date."')";
+  mysqli_query($mysqli, $query) or die('Error');
+  mysqli_insert_id($mysqli);
+  header("Location: movies.php?id=".$movie_id);
+  exit();
+ 
+}else{
+  header("Location: movies.php?id=".$movie_id);
+  $_SESSION['error']  = "Introduce un comentario.";
+  exit();
+}
+  // echo "<a href='/movies.php?id=".$movie_id."'>Volver</a>";
+  mysqli_close($mysqli);     
+?>        
--- www/movies.php
+++ www/movies.php
@@ -24,157 +24,32 @@ require __DIR__ . '/../php_util/db_connection.php';
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/styles.css">
+    <!-- <link rel="stylesheet" href="./assets/css/styles.css"> -->
+    <link rel="stylesheet" href="./assets/css/style_comment.css">
     <link rel="shortcut icon" href="#">
     <style>
-        /* .container {
+        .container {
             max-width: 1800px;
-        } */
-
-        /* @media(min-width:568px) {
-            .end {
-                margin-left: auto;
-            }
         }
-
-        @media(max-width:768px) {
-            #post {
-                width: 100%;
-            }
+        .movie_style{
+            padding: 0 !important;
+            width: 18.9rem;
+            margin: 14px;
+            position: relative;
+            background: #fff;
+            border: 2px solid #fff;
+            box-shadow: 0px 4px 7px rgba(0, 0, 0, .5);
+          
+            transition: all .5s cubic-bezier(.8, .5, .2, 1.4);
+            overflow: hidden;
+            height: 440px;
         }
-
-        #clicked {
-            padding-top: 1px;
-            padding-bottom: 1px;
-            text-align: center;
-            width: 100%;
-            background-color: #ecb21f;
-            border-color: #a88734 #9c7e31 #846a29;
-            color: black;
-            border-width: 1px;
-            border-style: solid;
-            border-radius: 13px;
-        }
-
-        #profile {
-            background-color: unset;
-
-        }
-
-        #post {
-            margin: 10px;
-            padding: 6px;
-            padding-top: 2px;
-            padding-bottom: 2px;
-            text-align: center;
-            background-color: #ecb21f;
-            border-color: #a88734 #9c7e31 #846a29;
-            color: black;
-            border-width: 1px;
-            border-style: solid;
-            border-radius: 13px;
-            width: 50%;
-        }
-
-        body {
-            background-color: black;
-        }
-
-        #nav-items li a,
-        #profile {
-            text-decoration: none;
-            color: rgb(224, 219, 219);
-            background-color: black;
-        }
-
-
-        .comments {
-            margin-top: 5%;
-            margin-left: 20px;
-        }
-
-        .darker {
-            border: 1px solid #ecb21f;
-            background-color: black;
-            float: right;
-            border-radius: 5px;
-            padding-left: 40px;
-            padding-right: 30px;
-            padding-top: 10px;
-        }
-
-        .comment {
-            border: 1px solid rgba(16, 46, 46, 1);
-            background-color: rgba(16, 46, 46, 0.973);
-            float: left;
-            border-radius: 5px;
-            padding-left: 40px;
-            padding-right: 30px;
-            padding-top: 10px;
-
-        }
-
-        .comment h4,
-        .comment span,
-        .darker h4,
-        .darker span {
-            display: inline;
-        }
-
-        .comment p,
-        .comment span,
-        .darker p,
-        .darker span {
-            color: rgb(184, 183, 183);
-        }
-
-        h1,
-        h4 {
-            color: white;
-            font-weight: bold;
-        }
-
-        label {
-            color: rgb(212, 208, 208);
-        }
-
-        #align-form {
-            margin-top: 20px;
-        }
-
-        .form-group p a {
-            color: white;
-        }
-
-        #checkbx {
-            background-color: black;
-        }
-
-        #darker img {
-            margin-right: 15px;
-            position: static;
-        }
-
-        .form-group input,
-        .form-group textarea {
-            background-color: black;
-            border: 1px solid rgba(16, 46, 46, 1);
-            border-radius: 12px;
-        }
-
-        form {
-            border: 1px solid rgba(16, 46, 46, 1);
-            background-color: rgba(16, 46, 46, 0.973);
-            border-radius: 5px;
-            padding: 20px;
-        } */
     </style>
     <title>MovieList</title>
 </head>
 
 <body class="bg-dark" style="background-image: url('./assets/images/movie-detail-bg.png');background-repeat: no-repeat;
     background-size: cover;">
-
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div class="container-fluid">
@@ -206,10 +81,9 @@ require __DIR__ . '/../php_util/db_connection.php';
                     </li>
 
                 </ul>
-                <form class="d-flex justify-content-end ms-2">
-                    <input class="form-control me-2 my-input" type="search" placeholder="Buscar peliculas"
-                        aria-label="Search">
-                    <button class="btn btn-primary btn-search" type="submit">Buscar</button>
+                <form class="d-flex justify-content-end ms-2" action="backend-search.php" method="GET">
+                <input class="form-control me-2 my-input"type="text" placeholder="Ejemplo: Sonic" name="query">
+		            <button class="btn btn-primary btn-search" type="submit" value="Search">Buscar</button>
                     <?php if (empty($_SESSION['user_id'])) {
                     ?>
                     <button class="btn btn-success btn-signin ms-2" type="submit"
@@ -233,7 +107,7 @@ require __DIR__ . '/../php_util/db_connection.php';
                                         }else{ 
                                             echo "<img width='35' height='35' class='rounded-circle' src='assets/imagenesUsuario/".$row['profile_image']."' >" ; 
                                         }
-                                    ?>
+                            ?>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
                             <a class="dropdown-item" href="profile.php">Panel</a>
@@ -249,6 +123,7 @@ require __DIR__ . '/../php_util/db_connection.php';
         </div>
     </nav>
 
+   
 
     <div class="container text-light">
         <div class="row default-row mt-1 mb-1" id="row-2">
@@ -260,80 +135,99 @@ require __DIR__ . '/../php_util/db_connection.php';
                 $query2 = 'SELECT * FROM tmovie WHERE id='.$movie_id;
                 $result2 = mysqli_query($mysqli, $query2) or die('Query error');
                 while($fila = mysqli_fetch_array($result2)){
-                    $variable= unserialize($fila['gender']);  
-                    echo '<h1>'.$fila['title'].'</h1>';
-                    echo "<img style='width:15%;' src='assets/imagenesPortada/".$fila['image']."' >";   
-                    echo '<p>'.$fila['sinopsis'].'</p>';
-                    echo '<p>'.$fila['created'].'</p>';
+                    $variable= unserialize($fila['gender']); 
+                    echo '<div class="container">';
+                    echo '<div class="row">';
+                    echo '<div class="col-md-3 mt-4 movie_style">';
+                    echo "<img style='width:100%;' src='assets/imagenesPortada/".$fila['image']."' >";
+                    echo '</div>';
+                    echo '<div class="col-md-7 mt-3">';
+                    echo '<h1>'.$fila['title'].'</h1>';   
+                    echo '<p >'.$fila['sinopsis'].'</p>';
+                    echo '<p>Fecha de lanzamiento: '.$fila['created'].'</p>';
+                    echo '<p>Duración: '.$fila['duration'].' </p>';
+                    echo '<h4>Géneros: </h4>';
                     foreach($variable as $value){
                         echo '<p class="mb-1">'.$value.' </p>';
                     }
-                    echo '<p>'.$fila['duration'].' </p>'; 
+                   
+                    echo '</div>';
+                    echo '</div>';
+                    echo '</div>';
+                  echo '<a href="add_to_watchlist.php?id='.$fila['id'].'" class="button btn-small">AÑADIR A LA WATCHLIST</a>';
+                    
                 }
             ?>
+             
         </div>
-
-        <div class="row default-row mt-1 mb-1" id="row-2">
-            <h3 >Comentarios:</h3>
-            <ul>
-                <?php
-                   $query3 = 'SELECT * FROM tComentarios WHERE movie_id='.$movie_id; 
-                // $query3 = 'SELECT tcomentarios.comentario, tuser.username FROM tcomentarios INNER JOIN tuser ON tcomentarios.id=tuser.id where tuser.id = '.$user_id.' and movie_id= '.$movie_id. '';
-                //   $sql = "SELECT especie.Nombre, animales.Animales FROM especie INNER JOIN animales ON especie.id=animales.IdEspecie where animales.IdEspecie=1";
-
-                    $result3 = mysqli_query($mysqli, $query3) or die('Query error');
-                    while ($row = mysqli_fetch_array($result3)) {
-                    echo 
-                    '<li> Comentario: '.$row['comentario']. ' || Fecha del comentario:  '.$row['fecha_comentario'].' || Nombre de usuario: </li>';
-                    }
-                mysqli_close($mysqli);
-                ?>
-            </ul>
-            <br>
-            <p>Deja un nuevo comentario:</p>
-            <form action="/comment.php" method="post">
-                <textarea rows="4" cols="50" name="new_comment"></textarea><br>
-                <input type="hidden" name="movie_id" value="<?php echo $movie_id; ?>">
-                <input type="submit" value="Comentar">
-            </form>
-        </div>
-        <!-- <section>
-            <div class="container">
+       
+       
+        <section>
+            
                 <div class="row">
                     <div class="col-sm-5 col-md-6 col-12 pb-4">
-                        <h1>Comentarios</h1>
+                        <h1 >Comentarios</h1>
+                        <?php
+                             //  $query3 = 'SELECT * FROM tComentarios WHERE movie_id='.$movie_id; 
+                            // $query3 = 'SELECT *, tuser.username, tuser.profile_image from tcomentarios INNER JOIN tuser ON tcomentarios.usuario_id = tuser.id';
+                            $query3 = 'SELECT *, tuser.username, tuser.profile_image FROM  tcomentarios INNER JOIN tuser ON tcomentarios.usuario_id = tuser.id 
+                            INNER JOIN tmovie  ON tcomentarios.movie_id = tmovie.id  WHERE movie_id = '.$movie_id;
+                            $result3 = mysqli_query($mysqli, $query3) or die('Query error');
+                           
+                        ?>
+                        <?php  
+                        while ($row = mysqli_fetch_array($result3)) {?>
                         <div class="comment mt-4 text-justify float-left">
-                            <img src="https://i.imgur.com/yTFUilP.jpg" alt="" class="rounded-circle" width="40"
-                                height="40">
-                            <h4>Nombree</h4>
-                            <span>FECHA</span>
+                            <?php 
+                                $profile_image = $row['profile_image'];
+                          
+                                if(empty($profile_image)){
+                                    $profile_image = "default-user.png";
+                                    echo "<img width='35' height='35' class='rounded-circle' src='assets/images/".$profile_image."' >" ;
+                                }else{ 
+                                    echo "<img width='35' height='35' class='rounded-circle' src='assets/imagenesUsuario/".$row['profile_image']."' >" ; 
+                                }
+                                
+                            ?>
+                            <h4><?php echo $row['username'] ?></h4>
+                            <span><?php echo $row['fecha_comentario'] ?></span>
                             <br>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus numquam assumenda hic
-                                aliquam vero sequi velit molestias doloremque molestiae dicta?</p>
+                            <p><?php echo $row['comentario'] ?></p>
                         </div>
-
+                    <?php }
+                     mysqli_close($mysqli);?>
                     </div>
                     <div class="col-lg-4 col-md-5 col-sm-4 offset-md-1 offset-sm-1 col-12 mt-4">
-                        <form id="algin-form">
-                            <div class="form-group">
-                                <h4>Leave a comment</h4>
-                                <label for="message">Message</label>
-                                <textarea name="msg" id="" msg cols="30" rows="5" class="form-control"
-                                    style="background-color: black;"></textarea>
+                        <form id="algin-form" action="comment.php" method="post">
+                            <div class="form-group" id="form-comment">
+                                <h4>Deja tu comentario</h4>
+                                <label for="message">Mensaje</label>
+                                <textarea  name="new_comment" id="" msg cols="30" rows="5" class="form-control  text-light"
+                                    style="background-color: #212529 ;"></textarea>
                             </div>
+                          
 
-
-                            <div class="form-group">
-                                <button type="button" id="post" class="btn">Post Comment</button>
+                            <div class="form-group" id="form-comment-div">
+                                <input type="hidden" name="movie_id" value="<?php echo $movie_id; ?>">
+                                <button type="submit" value="Comentar" class="btn btn-primary mt-2 p-2 mb-4">Comentar</button>
+                                <?php if(isset($_SESSION['error'])){
+                                ?>
+                                <div class="alert alert-danger text-center" style="margin-top:20px;">
+                                    <?php echo $_SESSION['error']; ?>
+                                </div>
+                                <?php
+                                    unset($_SESSION['error']);
+                                }?>
                             </div>
                         </form>
                     </div>
                 </div>
-            </div>
-        </section> -->
+               
+        </section>
+       
     </div>
 
-
+   
 
 
     <footer class="bg-dark text-center text-white ">
@@ -395,7 +289,7 @@ require __DIR__ . '/../php_util/db_connection.php';
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
     </script>
-    <script src="/assets/js/index.js" type="module"></script>
+   
 </body>
 
 </html>
